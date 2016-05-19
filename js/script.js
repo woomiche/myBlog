be418aee007f01c26ce279682bbc68e6fbb5fdd5
Michelle WOods <mwoods22@vt.edu>
2016-05-18 18:31:44 -0700

    pushing the site to gitHub with changes

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function() {
 var landingPage = {
 
   initialize: function () {
-    landingPage.matchBrowserHeight();
+    // landingPage.matchBrowserHeight();
   },
 
   matchBrowserHeight: function() {
